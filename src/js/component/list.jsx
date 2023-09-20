e321b9ff2f7cb26934f6a01301caa85769a53aa4
Adriana Carolina Vega Ahumada <carolinavegaahumada@gmail.com>
2023-09-20 20:47:49 +0000

    todo list guardo de tareas

--- src/js/component/list.jsx
+++ src/js/component/list.jsx
@@ -3,13 +3,14 @@ import {AiFillCloseCircle} from "react-icons/ai";
 
 
 const List = ( ) => {
-    const [elementlist, setElementList]=useState([{}]);
-    const [change, setChange]= useState({});
-    const [invisible, setinVisible]= useState([{label:"No hay tareas",done:false}]);
+    const [elementlist, setElementList]=useState([]);
+    const [change, setChange]= useState({label:""});
+    const [invisible, setinVisible]= useState([]);
+    const [numTask, setNumtask]=useState("");
+
     const url = 'https://playground.4geeks.com/apis/fake/todos/user/caro123';
 
     useEffect(() => {
-        console.log ("recarga");
         getlist();
         }, []);
 
@@ -18,8 +19,8 @@ const List = ( ) => {
     method: 'POST', // or 'POST'
     body: JSON.stringify([]), // los datos pueden ser una `cadena` o un {objeto} que proviene de algún lugar más arriba en nuestra aplicación
     headers:{
-    'Content-Type': 'application/json'
-    }
+    'Content-Type': 'application/json',
+    },
     })
     .then(res => {
         if (res.status>= 200 && res.status<=300){
@@ -33,15 +34,17 @@ const List = ( ) => {
         console.log(estadoUsuario)
     }
      )
-    .catch(error => console.log(error));
+    .catch(error => console.error(error));
     };
 
     const getlist = () =>{
+        const savedData = JSON.parse(localStorage.getItem('data'));
+        setElementList(savedData || []);
         fetch(url, {
         method: 'GET', // or 'POST'
         headers:{
-        'Content-Type': 'application/json'
-        }
+        'Content-Type': 'application/json',
+        },
         })
         .then(res => {
             if (res.status>= 200 && res.status<=300){
@@ -54,22 +57,21 @@ const List = ( ) => {
             }
         })
         .then(data => {
-            console.log(data); 
-            setElementList(data);
+            if (JSON.stringify(data) !== JSON.stringify(elementlist)) {
+               setElementList(data);
+            }
           })
-        .catch(error => console.log(error));
-    }    
+        .catch(error => console.error(error));
+    };    
     
 
 const update = (todos) =>{
-    console.log("prueba");
-    console.log(JSON.stringify(todos));
     fetch(url, {
-    method: 'PUT', // or 'POST'
-    body: JSON.stringify(todos), // los datos pueden ser una `cadena` o un {objeto} que proviene de algún lugar más arriba en nuestra aplicación
-    headers:{
-    'Content-Type': 'application/json'
-    }
+        method: 'PUT', // or 'POST'
+        body: JSON.stringify(todos), // los datos pueden ser una `cadena` o un {objeto} que proviene de algún lugar más arriba en nuestra aplicación
+        headers:{
+            'Content-Type': 'application/json',
+        },
     })
     .then(res => {
 	    if (res.status>= 200 && res.status<=300){
@@ -83,7 +85,8 @@ const update = (todos) =>{
     .then(data =>{
         console.log(data);
     })
-    .catch(error => console.log(error));
+    .catch(error => console.error(error));
+    numlist();
     };    
 
 function  handleOnChange (e) {
@@ -91,28 +94,37 @@ function  handleOnChange (e) {
 }   
 
 function handleKeyDown (event) {
-        if(event.key =='Enter')
+    if(event.key =='Enter')
         { 
+            const newLabel = change.label.trim();
+            if(newLabel !== ""){
             setElementList(elementlist.filter(element=>element.label!="No hay tareas" && element.label!="example task"))
             setElementList(current => [...current, change]);
-            setChange({label:"",done:false});
+            setChange({label:"",done:false});}
+            numlist();
     }
 }
 
 function eliminartarea(param){
-    const eliminar=elementlist[param]
-    setElementList(elementlist.filter(element=>element != eliminar))
+    const eliminar=elementlist[param];
+    setElementList(prevElementlist => prevElementlist.filter(element=>element != eliminar));
+    numlist();
 };
 
-function activedelete(event){
-    const index = event._targetInst.key
+function activedelete(ind){
+    const index = ind;
     let array = []
-    for(let i=0; i<elementlist.length; i++){
-        if(i==index){
-            array.push(1)
-        }else{array.push(0)}
-    }
+    if(elementlist[index].label==="No hay tareas"){
+        for(let i=0; i<elementlist.length; i++){
+            array.push(0);
+    }} else{
+        for(let i=0; i<elementlist.length; i++){
+            if(i==index){
+                array.push(1)
+            }else{array.push(0)}
+        }}
     setinVisible(array);
+    numlist();
 };
 
 function offdelete(){
@@ -121,11 +133,12 @@ function offdelete(){
         inv.push(0)        
     }
     setinVisible(inv);
-}
+    numlist();
+};
 
 useEffect(() => {
-    if(elementlist.length==0){
-        update({label:"No hay tareas",done:false})
+    if(elementlist.length===0 || elementlist.some((item)=>item.label==="No hay tareas")){
+        update([{label:"No hay tareas",done:false}]);
     }else{
    update(elementlist);}
   }, [elementlist]);
@@ -133,8 +146,23 @@ useEffect(() => {
 
 function handleClean(){
     setElementList([{label:"No hay tareas",done:false}]);
-    setChange({label:"",done:false});
+    
+}
 
+function numlist(){
+    let num ="";
+    if(elementlist.some((item) => item.label === "No hay tareas")){
+        num = "0 item left";
+            setNumtask(num);
+          } else if(elementlist.length ===1){
+            num = "1 item left";
+          }
+          else{
+            num = `${elementlist.length} item left`;
+           
+          }
+          setNumtask(num); 
+    
 }
 	return (
             
@@ -144,14 +172,14 @@ function handleClean(){
                     </div>
                     <div className="tasktext">
                         <ul>
-                            {elementlist?.map((thingdo, index)=><div className={"element py-2 "} key={index} onMouseEnter={activedelete} onMouseLeave={offdelete}>
+                            {elementlist?.map((thingdo, index)=><div className={"element py-2 "} key={index} onMouseEnter={()=>activedelete(index)} onMouseLeave={()=>offdelete()}>
                                 <li className="list pl-1">{thingdo.label}</li>
                                 <div className="delete" id={index} style={{opacity:invisible.length != 0 ? invisible[index]:0 }}  onClick={()=>eliminartarea(index)} ><AiFillCloseCircle/></div>
                             </div>)}
                         </ul>
                     </div>
                     <div className="numbertasks p-1">
-                        <p>{elementlist.length} item left</p>
+                        <p>{numTask}</p>
                     </div>
                     <div className ="contentbutton p-2">
                         <button className="btn btn-secondary" onClick={()=>handleClean() }>Clean Tasks</button>
